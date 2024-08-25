d6405cd8cc03c23e35c20be79c020df61658edc5
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-08-25 17:54:18 +0100

    Add Python script to show GH repo languages

--- analyser/main.py
+++ analyser/main.py
@@ -1,4 +1,17 @@
+from github import Github
+
+
 def main() -> None:
-    """Main function."""
-    print("Hello World!")
-    print("Hello World2!")
+    """Entrypoint for Application."""
+    try:
+        github = Github()
+        github_stats = github.get_repo("JackPlowman/github-stats")
+        print(github_stats.get_languages())
+    except Exception as e:
+        print(e)
+    finally:
+        github.close()
+
+
+if __name__ == "__main__":
+    main()
