f648c43a3ae617ded0800a3bd9f05b6613b05c03
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-08-26 16:46:30 +0100

    Update

--- analyser/application/tests/test_main.py
+++ analyser/application/tests/test_main.py
@@ -8,9 +8,7 @@ FILE_PATH = "application.main"
 @patch(f"{FILE_PATH}.create_markdown_file")
 @patch(f"{FILE_PATH}.add_languages_sloc_table")
 @patch(f"{FILE_PATH}.set_up_markdown_file")
-@patch(f"{FILE_PATH}.Github")
 def test_main(
-    mock_github: MagicMock,
     mock_set_up_markdown_file: MagicMock,
     mock_add_languages_sloc_table: MagicMock,
     mock_create_markdown_file: MagicMock,
@@ -18,7 +16,6 @@ def test_main(
     # Act
     main()
     # Assert
-    mock_github.assert_called_once()
     mock_set_up_markdown_file.assert_called_once_with("index", "GitHub Stats Repository")
     mock_add_languages_sloc_table.assert_called_once_with(mock_set_up_markdown_file.return_value)
     mock_create_markdown_file.assert_called_once_with(mock_add_languages_sloc_table.return_value)
