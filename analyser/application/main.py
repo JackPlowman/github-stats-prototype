23fe756fcebd29cf8dbec736444df140b14a56c8
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-08-28 18:09:53 +0100

    Add more logging

--- analyser/application/main.py
+++ analyser/application/main.py
@@ -1,4 +1,5 @@
-from structlog import contextvars, get_logger, stdlib
+from structlog import get_logger, stdlib
+from structlog.contextvars import bind_contextvars, unbind_contextvars
 
 from .custom_logging import set_up_custom_logging
 from .markdown.markdown import set_up_index_page
@@ -15,12 +16,14 @@ def main() -> None:
         repositories = ["JackPlowman/github-stats"]
         repositories_stats = []
         for repository in repositories:
-            contextvars.bind_contextvars(repository=repository)
+            bind_contextvars(repository=repository)
             logger.info("Analysing repository", repository=repository)
             total_files = analyse_repository(repository)
             repositories_stats.extend(
                 [Repository(repository, "A repository for analysing GitHub repositories.", total_files)]
             )
+        unbind_contextvars("repository")
+
         set_up_index_page(repositories_stats)
     except Exception:
         logger.exception("An error occurred during the execution of the analyser.")
