cf7b943afa1395a7c83ff09afbd46f2ad912b5ce
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-08-28 19:13:06 +0100

    Improve coverage

--- analyser/application/tests/test_github_interactions.py
+++ analyser/application/tests/test_github_interactions.py
@@ -1,6 +1,6 @@
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock, call, patch
 
-from application.github_interactions import clone_repo
+from application.github_interactions import clone_repo, retrieve_repositories
 
 FILE_PATH = "application.github_interactions"
 
@@ -27,3 +27,34 @@ def test_clone_repo_exists(mock_repo: MagicMock, mock_path: MagicMock) -> None:
     clone_repo("JackPlowman", "github-stats")
     # Assert
     mock_repo.clone_from.assert_not_called()
+
+
+@patch(f"{FILE_PATH}.Github")
+@patch(f"{FILE_PATH}.getenv")
+def test_retrieve_repositories__unauthenticated(mock_getenv: MagicMock, mock_github: MagicMock) -> None:
+    # Arrange
+    mock_getenv.side_effect = ["Test", ""]
+    full_name = "Test1/Test2"
+    mock_github.return_value.search_repositories.return_value = [MagicMock(full_name=full_name)]
+    # Act
+    repositories = retrieve_repositories()
+    # Assert
+    mock_github.assert_called_once_with()
+    mock_getenv.assert_has_calls([call("REPOSITORY_OWNER", ""), call("GITHUB_TOKEN", "")])
+    assert repositories == [full_name]
+
+
+@patch(f"{FILE_PATH}.Github")
+@patch(f"{FILE_PATH}.getenv")
+def test_retrieve_repositories__authenticated(mock_getenv: MagicMock, mock_github: MagicMock) -> None:
+    # Arrange
+    token = "TestToken"  # noqa: S105
+    mock_getenv.side_effect = ["Test", token]
+    full_name = "Test3/Test4"
+    mock_github.return_value.search_repositories.return_value = [MagicMock(full_name=full_name)]
+    # Act
+    repositories = retrieve_repositories()
+    # Assert
+    mock_github.assert_called_once_with(token)
+    mock_getenv.assert_has_calls([call("REPOSITORY_OWNER", ""), call("GITHUB_TOKEN", "")])
+    assert repositories == [full_name]
