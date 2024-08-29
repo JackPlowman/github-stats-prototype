9e34bab0412bca9a6d30e2ef3ba5c31cb726499f
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-08-29 12:34:36 +0100

    Refactor python code structure

--- analyser/application/github_interactions.py
+++ analyser/application/github_interactions.py
@@ -4,7 +4,7 @@ from os import getenv
 from pathlib import Path
 
 from git import Repo
-from github import Github
+from github import Github, PaginatedList, Repository
 from structlog import get_logger, stdlib
 
 logger: stdlib.BoundLogger = get_logger()
@@ -31,11 +31,11 @@ def clone_repo(owner_name: str, repository_name: str) -> str:
     return file_path
 
 
-def retrieve_repositories() -> list[object]:
+def retrieve_repositories() -> PaginatedList[Repository]:
     """Retrieve the list of repositories to analyse.
 
     Returns:
-        list[object]: The list of repositories.
+        PaginatedList[Repository]: The list of repositories.
     """
     user = getenv("REPOSITORY_OWNER", "")
     if user == "":
---
+++ analyser/application/pages/pages.py
@@ -0,0 +1,17 @@
+from structlog import get_logger, stdlib
+
+from application.github_interactions import retrieve_repositories
+
+from .index_page import set_up_index_page
+from .repository_page import set_up_repository_pages
+
+logger: stdlib.BoundLogger = get_logger()
+
+
+def create_markdown_pages() -> None:
+    """Create the markdown pages."""
+    repositories = retrieve_repositories()
+    repositories_stats = []
+    repositories_stats = set_up_repository_pages(repositories, repositories_stats)
+
+    set_up_index_page(repositories_stats)
